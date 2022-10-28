55b09506efd22cb11a2655ffe1e93cf63b05449e
Lukasz0104 <lukasz.01.stepien@gmail.com>
2022-10-29 00:39:16 +0200

    Basic Spring Boot project configuration

---
+++ Guesthouse-spring-boot/src/main/java/pl/lodz/p/it/pas/GuesthouseApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.pas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuesthouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuesthouseApplication.class, args);
+	}
+
+}
---
+++ Guesthouse-spring-boot/src/test/java/pl/lodz/p/it/pas/GuesthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.pas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesthouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
