bcca3e41959e955ddd2bacaa0652d4cc807d7323
rstrzalkowski <resumc@wp.pl>
2022-11-09 16:21:36 +0100

    Initial MVC project

---
+++ Guesthouse-MVC/src/main/java/pl/lodz/p/it/pas/guesthousemvc/HelloApplication.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.it.pas.guesthousemvc;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
