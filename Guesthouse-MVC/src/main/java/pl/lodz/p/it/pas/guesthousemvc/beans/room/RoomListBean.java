32940853a76017140222435541d958c3d5dbf7ac
Lukasz0104 <lukasz.01.stepien@gmail.com>
2022-11-23 18:52:27 +0100

    Disable button when changing past or ongoing rent

--- Guesthouse-MVC/src/main/java/pl/lodz/p/it/pas/guesthousemvc/beans/room/RoomListBean.java
+++ Guesthouse-MVC/src/main/java/pl/lodz/p/it/pas/guesthousemvc/beans/room/RoomListBean.java
@@ -1,18 +1,18 @@
 package pl.lodz.p.it.pas.guesthousemvc.beans.room;
 
 
-import lombok.Getter;
-import pl.lodz.p.it.pas.guesthousemvc.restClients.RoomRESTClient;
-import pl.lodz.p.it.pas.model.Room;
-
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import pl.lodz.p.it.pas.guesthousemvc.restClients.RoomRESTClient;
+import pl.lodz.p.it.pas.model.Room;
 
 @Named
 @ViewScoped
@@ -44,5 +44,9 @@ public class RoomListBean implements Serializable {
             //TODO display error message
         }
         refreshRoomList();
+    }
+
+    public boolean getPast(LocalDateTime dateTime) {
+        return dateTime.isBefore(LocalDateTime.now());
     }
 }
